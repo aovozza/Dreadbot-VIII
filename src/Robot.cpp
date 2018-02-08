e510334c451d6c756b380daa1f9822ff236d512c
Clara Gauthier <cegauthier1@gmail.com>
2018-02-08 18:21:03 -0500

    "Add author name Clara"

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -6,7 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 //Dreadbot VIII - FIRST Power Up
-//Authors: Christian Vaughan, Ethan Leonello, Clara Gauthier,
+//Authors: Christian Vaughan, Ethan Leonello, Clara Gauthier
 
 #include <iostream>
 #include <string>
